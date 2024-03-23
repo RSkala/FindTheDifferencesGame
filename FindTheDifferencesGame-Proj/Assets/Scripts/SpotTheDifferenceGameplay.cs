883db96f39a288a83288167d7d73085de83475e5
RSkala <RichardSkala@gmail.com>
2024-03-22 22:45:17 -0700

    Handle OnClick InputAction. Added Colliders to selector prefabs.

--- FindTheDifferencesGame-Proj/Assets/Scripts/SpotTheDifferenceGameplay.cs
+++ FindTheDifferencesGame-Proj/Assets/Scripts/SpotTheDifferenceGameplay.cs
@@ -1,16 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SpotTheDifferenceGameplay : MonoBehaviour
 {
+    Camera mainCamera;
+    
     void Start()
     {
-        
+        mainCamera = Camera.main;
     }
-    
-    void Update()
+
+    void OnClick(InputValue inputValue)
     {
-        
+        Vector3 mousePosition = Mouse.current.position.value;
+        mousePosition.z = mainCamera.nearClipPlane;
+        Vector3 mouseWorldPoint = mainCamera.ScreenToWorldPoint(mousePosition);
+
+        RaycastHit2D raycastHit2D = Physics2D.Raycast(mouseWorldPoint, mainCamera.transform.forward);
+        if(raycastHit2D.collider != null)
+        {
+            GameObject clickedGameObject = raycastHit2D.collider.gameObject;
+            Debug.Log("clickedGameObject: " + clickedGameObject.name);
+        }
     }
 }
